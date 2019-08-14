f27a0f24020ce6e94e00fedc905f32bf4ef938ef
ajacob7 <39599106+ajacob7@users.noreply.github.com>
2019-08-13 18:28:39 -0700

    Download is working

--- camino.py
+++ camino.py
@@ -1,4 +1,3 @@
-import os
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
@@ -8,49 +7,97 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By #used for private xpath call method
 from selenium.webdriver.support.ui import Select #used to select a button
 from selenium.webdriver.common.keys import Keys #used to select a button
-from selenium.webdriver.chrome.options import Options #used for headless browser function
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+
 import getpass
 import time
 import sys
+import os
+import pathlib
+import zipfile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+save_location = pathlib.Path(BASE_DIR + "/SCU")
+zipFolder = "course_files_export.zip"
 
 if sys.platform ==  'win32':
 	chromedriver_location = os.path.join(BASE_DIR, 'chromedriver')
 else:
 	chromedriver_location = os.path.join(BASE_DIR, 'mac_chromedriver')
-#Setup driver
+
+def filesExist(course):
+	try:
+		driver.find_element_by_class_name('files')
+	except NoSuchElementException:
+		print("No files tab for " + course + ".")
+		return False
+	return True
+
+
+
+# Setup driver
 driver = webdriver.Chrome(chromedriver_location)
-# driver = webdriver.Chrome()
 username = "ajacob1"
 
-#Login
+# Login
 driver.get("https://camino.instructure.com")
 driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
-pw = getpass.getpass()
+# pw = getpass.getpass()
+pw = "Qigz-d4dd-mvli"
 driver.find_element_by_xpath('//*[@id="password"]').send_keys(pw + "\n")
 
-#Navigate to courses
+# Navigate to courses
 driver.find_element_by_xpath('//*[@id="global_nav_courses_link"]').click()
 time.sleep(1)
+# All courses
 driver.find_element_by_xpath('//*[@id="nav-tray-portal"]/span/span/div/div/div/div/ul/li[3]/a').click()
-
 time.sleep(1)
-courses = driver.find_elements_by_xpath("//a[@href]")
 
+# Get all courses
+links = driver.find_element_by_xpath('//*[@id="content"]').find_elements_by_xpath('.//a[@href]')
+# Convert courses to list of hrefs
+courses = []
+for each in links:
+	courses.append(each.get_attribute('href'))
+	
 for course in courses:
-	print(course)
-	#navigate to courses
-	# if :#exists file subsection
-	# 	#navigate to files
-	# 	for file in files:
+	driver.get(course)
+	time.sleep(2)
+	courseName = driver.find_elements_by_xpath('.//a[@href]')
+	courseName = courseName[11].text.split('-')[0]
+	print(courseName)
+	time.sleep(1)
+
+	#if theres a files section
+	if filesExist(courseName):
+		os.mkdir(courseName)
+		driver.find_element_by_class_name('files').click()
+		time.sleep(2)
+
+		# docs = driver.find_elements_by_class_name('al-dropdown__container')
+		docs = driver.find_elements_by_class_name('icon-more')
+
+		# driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/div/div/div/div[5]/div[6]/div[2]/button')
+		print("# of docs = ", len(docs))
+		for doc in docs:
+			doc.click()
+			time.sleep(.3)
+			dl = driver.find_element_by_link_text('Download')
 			
-	# else:
-	# 	continue
+			if "files#" in dl.get_attribute('href'):
+				dl.click()
+				while(not pathlib.Path(zipFolder).exists()):
+					time.sleep(.2)
+
+				with zipfile.ZipFile(currentDir+ "\\" + zipFolder, "r") as zip_ref:
+					zip_ref.extractall(currentDir)
+				os.remove(zipFolder)
 
 
 
-
-
-# currentFolder
-# os.rename("C:/Users/asaja/Downloads/"+currentFile, currentFolder + "/" + currentFile)
+			doc.click()
+			# dl.click()
+			time.sleep(.3)
+	else:
+		continue
